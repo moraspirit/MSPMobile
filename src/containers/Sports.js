ba5a354ec9bd15038f5d08bfc6b6144a09a7a0d0
Heshan Padmasiri <heshanpadmasiri@hotmail.com>
2018-09-02 06:00:17 +0530

    Added icon based list

--- src/containers/Sports.js
+++ src/containers/Sports.js
@@ -1,58 +1,91 @@
 import React, { Component } from 'react';
-import { Modal, View, ScrollView, Text, StyleSheet, TouchableHighlight, Button, Alert } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { Button } from 'react-native-elements'
+import { Icon } from 'react-native-elements'
 import call from 'react-native-phone-call';
-import { Col, Row, Grid } from "react-native-easy-grid";
+import Accordion from 'react-native-collapsible/Accordion';
+
 class Sports extends Component {
 
-    state = {
-        modalVisible: false,
-    };
+    sports=[
+        {
+            title:'Title1',
+            venue:'Venue1',
+            time:'Time1',
+            captain:'Captain1',
+            viceCaptian:'ViceCaptian1',
+            contact:'0372239161',
+            modalVisible:false,
+            icon:'run'
+        },
+        {
+            title:'Title2',
+            venue:'Venue2',
+            time:'Time2',
+            captain:'Captain2',
+            viceCaptian:'ViceCaptian2',
+            contact:'0766041559',
+            modalVisible:false,
+            icon:'run'
+        }
+    ]
 
-    setModalVisible(visible){
-        this.setState({modalVisible: visible});
+    _renderTitle(sport){
+        return (
+            <View style={styles.title}>
+                <Icon
+                    type='material-community'
+                    name={sport.icon}
+                    size={35}
+                />
+                <Text style = {styles.heading}>{sport.title}</Text>
+            </View>
+        )
+    }
+
+    _renderSectionTitle(sport){
+        return (
+            <View>
+                <Text>{sport.title}</Text>
+            </View>
+        )
+    }
+
+    _renderContent(sport){
+        return (
+            <View style = {styles.popup}>
+                <Text style = {styles.para}>Venue:{sport.venue}</Text>
+                <Text style = {styles.para}>Time:{sport.time}</Text>
+                <Text style = {styles.para}>Captain:{sport.captain}</Text>
+                <Text style = {styles.para}>vice-Captain:{sport.viceCaptian}</Text>
+                <Text style = {styles.para}>Contact:{sport.contact}</Text>
+                <Button
+                    raised
+                    icon={{name: 'phone',type: 'material-community'}}
+                    onPress={() => {
+                        Sports.makeCall(sport.contact);
+                    }}
+                    title="Contact"
+                /> 
+            </View>
+        )
+    }
+
+    static makeCall(number){
+        const args = {
+            number: number, // String value with the number to call
+            prompt: true // Optional boolean property. Determines if the user should be prompt prior to the call 
+        }                            
+        call(args).catch(console.error)
     }
 
     render() {
         return (
-            <ScrollView style={styles.container}>
-                <View style={styles.container}>
-                    <Modal
-                        animationType="slide"
-                        transparent={true}
-                        visible={this.state.modalVisible}
-                        onRequestClose={() => {
-                            this.setModalVisible(!this.state.modalVisible);
-                        }}>
-                        <View style={styles.popup}>                                          
-                            <Text style = {styles.title}>Athletics (Male)</Text>
-                            <Text style = {styles.para}>Venue    :Ground</Text>
-                            <Text style = {styles.para}>Time    : 6.00pm Every Monday</Text>
-                            <Text style = {styles.para}>Captain   : H.A.H.C.Hettiarachchi</Text>
-                            <Text style = {styles.para}>vice-Captain : G.P.M.Lakmal</Text>
-                            <Text style = {styles.para}>Contact : 0702701550</Text>
-                            <Button
-                                onPress={() => {
-                                    const args = {
-                                        number: '0372239161', // String value with the number to call
-                                        prompt: true // Optional boolean property. Determines if the user should be prompt prior to the call 
-                                    }                            
-                                    call(args).catch(console.error)
-                                }}
-                                title="Press Me"
-                            /> 
-                        </View>
-                    </Modal>
-                    <Button 
-                        onPress={() => {
-                            this.setModalVisible(!this.state.modalVisible)
-                        }}
-                        title="Modal"/>
-                </View>
-                
-        
-                
-
-            </ScrollView>
+            <Accordion
+                sections={this.sports}                
+                renderHeader={this._renderTitle}
+                renderContent={this._renderContent}
+            />
         );
     }
 }
@@ -70,15 +103,19 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         elevation: 2,
     },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        textAlign: 'left',
-        margin: 10
+    title: {        
+        flexDirection:'row',
+        justifyContent: 'space-around',
+        padding: 10
+    },
+    heading: {
+        fontSize: 35,
+        lineHeight: 40
     },
     para: {
         fontSize: 16,
-        lineHeight: 25
+        lineHeight: 25,
+        textAlign:'center'
     },
     popup: {
         flex: 1,
@@ -86,8 +123,8 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         padding: 20,
-        marginHorizontal:25,
-        marginVertical:120,
+        marginHorizontal:5,
+        marginVertical:5,
         backgroundColor: '#fff',
         elevation: 2,
     }
